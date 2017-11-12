21a4c31d8557542f046bc59781b72708cf96a084
MassKonFuzion <6656037+masskonfuzion@users.noreply.github.com>
2017-11-12 02:29:22 -0600

    Vectorized the Shots class (still a little bit of work to do before integrating colliders, but getting close)

--- js/Shots.js
+++ js/Shots.js
@@ -4,46 +4,51 @@ function shotClass() {
 	cannonShotSpeed = 6;
 	cannonReloadFrames = 5;
 	cannonReloadLeft = 0;
-	this.x = cannonEndX;
-	this.y = cannonEndY;
+	this.position = vec2.create(cannonEndX, cannonEndY);
+
 	this.moveAng = cannonAngle;
 	this.speed = cannonShotSpeed;
 	this.removeMe = false;
 
 	this.draw = function() {
 		canvasContext.fillStyle = "yellow";
-		canvasContext.fillRect(this.x-1,this.y-1,3,3);
+		canvasContext.fillRect(this.position.x-1,this.position.y-1,3,3);
 	}
 
 	this.move = function() {
-		this.x += this.speed * Math.cos(this.moveAng);
-		this.y += this.speed * Math.sin(this.moveAng);
+		// TODO create a velocity property of shotClass. Use that, instead of computing shotVel in move()
+		var shotVel = vec2.create(Math.cos(this.moveAng), Math.sin(this.moveAng));
+		vec2.scale(shotVel, shotVel, this.speed);
+		vec2.add(this.position, this.position, shotVel);
 	}
 
 	this.shotCollisionAndBoundaryCheck = function() {
 		// note: not checking screen bottom since we can't shoot down
-		if(this.x<0 || this.x>canvas.width || this.y<0) {
+		if(this.position.x<0 || this.position.x>canvas.width || this.position.y<0) {
 			this.removeMe = true;
 		}
 
 		for(var e=0;e<shipList.length;e++) {
-			if(this.y > shipList[e].position.y-shipHeight/2 && this.y < shipList[e].position.y+shipHeight/2 &&
-			   this.x > shipList[e].position.x-shipWidth/2 && this.x < shipList[e].position.x+shipWidth/2) {
-			   
-			   score += scoreForShipShot;
-			   shipList[e].removeMe=true;
-			   this.removeMe = true;
+			// TODO replace with line segment/aabb intersection test (use shot velocity to compute previous known position; make line seg from last-known to current position)
+			if(this.position.y > shipList[e].position.y-shipHeight/2 && this.position.y < shipList[e].position.y+shipHeight/2 &&
+			   this.position.x > shipList[e].position.x-shipWidth/2 && this.position.x < shipList[e].position.x+shipWidth/2) {
+
+				score += scoreForShipShot;
+				shipList[e].removeMe=true;
+				this.removeMe = true;
 			}
 		}
 		for(var t=0;t<alienList.length;t++) {
-			if(this.y > alienList[t].position.y-alienHeight && this.y < alienList[t].position.y &&
-			   this.x > alienList[t].position.x-alienWidth/2 && this.x < alienList[t].position.x+alienWidth/2) {
+			// TODO replace with line segment/aabb intersection test (use shot velocity to compute previous known position; make line seg from last-known to current position)
+			if(this.position.y > alienList[t].position.y-alienHeight && this.position.y < alienList[t].position.y &&
+			   this.position.x > alienList[t].position.x-alienWidth/2 && this.position.x < alienList[t].position.x+alienWidth/2) {
 			   
 			   score += scoreForAlienShot;
 			   alienList[t].removeMe=true;
 			   this.removeMe = true;
-			} else if(this.y > alienList[t].chuteY && this.y < alienList[t].chuteY+parachuteH &&
-                      this.x > alienList[t].chuteX && this.x < alienList[t].position.x+parachuteW && alienList[t].isChuteDrawn) {
+			} else if(this.position.y > alienList[t].chuteY && this.position.y < alienList[t].chuteY+parachuteH &&
+                      this.position.x > alienList[t].chuteX && this.position.x < alienList[t].position.x+parachuteW && alienList[t].isChuteDrawn) {
+					// TODO replace with line segment/aabb intersection test (use shot velocity to compute previous known position; make line seg from last-known to current position)
 			   	
 			   	score += scoreForParachuteShot;
 			   	alienList[t].isChuteDrawn = false;
