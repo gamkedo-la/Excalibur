8e73a563d0f70a73781302f136c5fa420d836044
MassKonFuzion <6656037+masskonfuzion@users.noreply.github.com>
2017-11-13 22:59:59 -0600

    Changed "extents" to "halfSizes" in the aabb class (hopefully makes the name/purpose more clear). Also added a couple of helper functions

--- js/Aabb.js
+++ js/Aabb.js
@@ -1,8 +1,8 @@
 function aabb(width = 0, height = 0) {
 	this.center = vec2.create();
 
-	// extents in an AABB are the half-widths along each axis
-	this.extents = [width, height];		// Store half-widths in an array to keep data contiguous
+	// halfSizes in an AABB are the half-widths along each axis
+	this.halfSizes = [width, height];		// Store half-widths in an array to keep data contiguous
 
 	this.minPt = vec2.create();
 	this.maxPt = vec2.create();
@@ -15,15 +15,25 @@ function aabb(width = 0, height = 0) {
 		this.maxPt.x = this.center.x + this.width;
 		this.maxPt.y = this.center.y + this.height;
 	};
+
+	this.setCenter = function(x, y) {
+		vec2.set(this.center, x, y);
+	}
+
+	// Set half-sizes
+	this.setHalfSizes = function(x, y) {
+		this.width = x;
+		this.height = y;
+	}
 }
 
-// getter/setter properties, to allow developers to work with aabb.width and aabb.height, instead of aabb.extents[0], and aabb.extents[1]
+// getter/setter properties, to allow developers to work with aabb.width and aabb.height, instead of aabb.halfSizes[0], and aabb.halfSizes[1]
 Object.defineProperty(aabb.prototype, 'width', {
-	get: function() { return this.extents[0]; },
-	set: function(inp) { this.extents[0] = inp; }
+	get: function() { return this.halfSizes[0]; },
+	set: function(inp) { this.halfSizes[0] = inp; }
 });
 
 Object.defineProperty(aabb.prototype, 'height', {
-	get: function() { return this.extents[1]; },
-	set: function(inp) { this.extents[1] = inp; }
+	get: function() { return this.halfSizes[1]; },
+	set: function(inp) { this.halfSizes[1] = inp; }
 });
