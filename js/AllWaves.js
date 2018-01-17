159aabae9b3dc9896955fcb617bd16f87e205ff4
Terrence <t.w.mcdonnell10@gmail.com>
2018-01-16 16:03:48 -0800

    New wave added to stage 1 + AllWaves has comments showing where each stage starts and ends

--- js/AllWaves.js
+++ js/AllWaves.js
@@ -2,6 +2,8 @@ const PLANE_PARADROPPER = 1;
 const PLANE_GUNSHIP = 2;
 const MISSILE_STRIKE = 3;
 
+// STAGE 1 \\
+
 var stage1WaveNumber1 = [
     { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 30 },
     { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 5 },
@@ -20,6 +22,67 @@ var stage1WaveNumber2 = [
     { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 5 }
 ];
 
+var stage1WaveNumber3 = [ 
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 16 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 17 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 16 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 16 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 16 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 17 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 16 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 17 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 16 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 17 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 16 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 17 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 16 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 16 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 16 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 16 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 16 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 17 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 16 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 15 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 17 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 14 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 18 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 18 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 16 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 16 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 16 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 18 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 16 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 17 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 16 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 14 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 15 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 18 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 17 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 18 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 16 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 17 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 14 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 18 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 17 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 17 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 17 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 14 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 16 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 18 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 16 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 16 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 16 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 17 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 15 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 17 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 16 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 17 },
+    { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 17 },
+    { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 15 }
+];
+
+// STAGE 2 \\
+
 var stage2WaveNumber1 = [
     { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 30 },
     { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 5 },
@@ -29,6 +92,8 @@ var stage2WaveNumber1 = [
     { spawnType: PLANE_GUNSHIP, framesUntilSpawn: 5 },
     { spawnType: PLANE_PARADROPPER, framesUntilSpawn: 10 }
 ];
+
+// STAGE 3 \\
 
 var stage3WaveNumber1 = [ 
     { spawnType: MISSILE_STRIKE, framesUntilSpawn: 7 },
