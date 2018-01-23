654a50db8851569780187158bead2d8b7466b47c
ashsimmonds <ashsimmonds@gmail.com>
2018-01-22 18:40:09 -0600

    Added Galaga'esque stats to game over screen.

--- js/gameOver.js
+++ js/gameOver.js
@@ -49,9 +49,14 @@ function gameOverSequence() {
 
       if (artHeightTopNow >= canvas.height) {
         if(framesSinceGameOverShown<0) {
-          drawStroked('GAME OVER!!', canvas.width / 6, canvas.height / 2, 'red' , '80px Sans-serif')
-          drawStroked('Final Score :'+ score, canvas.width / 2, canvas.height / 2 + 80, 'white' , '60px Sans-serif','center');
-            drawStroked(" [  P R E S S  E N T E R  ]", canvas.width/2, canvas.height/2 + 140, "white", "30px arial", "center");
+          drawStroked("GAME OVER!!", canvas.width / 6, canvas.height / 4, "red", "80px Sans-serif")
+          drawStroked("Final Score :" + score, canvas.width / 2, canvas.height / 4 + 80, "white", "60px Sans-serif", "center");
+          drawStroked("[ P R E S S  E N T E R ]", canvas.width / 2, canvas.height / 4 + 140, "white", "30px arial", "center");
+
+          drawStroked("Shots Fired: "+ shotsFired, canvas.width / 2, canvas.height / 4 + 260, "white", "30px Sans-serif", "center");
+          drawStroked("Number Of Hits: "+ shotsHit, canvas.width / 2, canvas.height / 4 + 320, "white", "30px Sans-serif", "center");
+          drawStroked("Hit-Miss Ratio: "+ (shotsHit / shotsFired * 100).toFixed(1) + "%", canvas.width / 2, canvas.height / 4 + 380, "white", "30px Sans-serif", "center");
+
           framesSinceGameOverShown = framesShowingGameOverTextBeforeReset;
         } /*else if(framesSinceGameOverShown-- == 0) {
           this.gameOverPlaying = false;
